f57dde1f333de5ad82ff4b0154dedf409f79f078
Chip Wickings <34577860+Chip-W@users.noreply.github.com>
2020-11-12 06:33:58 -0500

    English SQL

---
+++ Build/sql_en.sql
@@ -0,0 +1,19 @@
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('a_c_fishbluegil_01_ms', 'Medium Bluegil', 3, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('a_c_fishbluegil_01_sm', 'Small Bluegil', 5, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('a_c_fishbullheadcat_01_ms', 'Medium Bullhead Catfish', 3, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('a_c_fishbullheadcat_01_sm', 'Small Bullhead Catfish', 5, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('a_c_fishchainpickerel_01_ms', 'Medium Chain Pickerel', 3, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('a_c_fishchainpickerel_01_sm', 'Small Chain Pickerel', 5, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('a_c_fishlargemouthbass_01_ms', 'Large Mouth Bass', 3, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('a_c_fishperch_01_ms', 'Medium Perch', 3, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('a_c_fishperch_01_sm', 'Small Perch', 5, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('a_c_fishrainbowtrout_01_ms', 'Rainbow Trout', 3, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('a_c_fishredfinpickerel_01_ms', 'Medium Red Fin Pickerel', 3, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('a_c_fishredfinpickerel_01_sm', 'Small Red Fin Pickerel', 5, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('a_c_fishrockbass_01_ms', 'Medium Rock Bass', 3, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('a_c_fishrockbass_01_sm', 'Small Rock Bass', 5, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('a_c_fishsalmonsockeye_01_ms', 'Sockeye Salmon', 3, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('a_c_fishsmallmouthbass_01_ms', 'Small Mouth Bass', 3, 1, 'item_standard', 0);
+
+
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('fishbait', 'Bait', 10, 1, 'item_standard', 1);
